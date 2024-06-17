fa6a863831b2d314dafe2612976f3ea2025c22af
Катерина Бельчинська <72660010+kbelchynska@users.noreply.github.com>
2024-06-17 08:57:39 +0000

    add server side validation

---
+++ app/meals/share/error.js
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Error({ error }) {
+  return (
+    <main className="not-found">
+      <h1>An error occured!</h1>
+      <p>Failed to create a meal</p>
+    </main>
+  );
+}
